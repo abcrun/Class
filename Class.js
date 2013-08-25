fe480d1063aeb8b2db1a60e5bd6f8d55ecd5dac4
Hongbo Yang <abcrun@gmail.com>
2013-08-25 13:33:42 +0800

    Format

--- Class.js
+++ Class.js
@@ -5,70 +5,70 @@
 */
 
 (function(name,factory){
-	if(typeof define === 'function' && define.amd) define(factory);//AMD
-	else if(typeof module === 'object' && module.exports) module.exports = factory();//CommonJS
-	else this[name] = factory();//Global
+    if(typeof define === 'function' && define.amd) define(factory);//AMD
+    else if(typeof module === 'object' && module.exports) module.exports = factory();//CommonJS
+    else this[name] = factory();//Global
 })('Class',function(){
-	var type = function(arg){
-			return /\s(\w+)/.exec(Object.prototype.toString.call(arg).toLowerCase())[1];
-	};
+    var type = function(arg){
+        return /\s(\w+)/.exec(Object.prototype.toString.call(arg).toLowerCase())[1];
+    };
 
-	var _implement = function(_extends){
-		for(key in _extends) this[key] = _extends[key];
-		return this;
-	}
+    var _implement = function(_extends){
+        for(key in _extends) this[key] = _extends[key];
+        return this;
+    }
 
-	var klass = function(_constructor,_extends){
-		var _prop,constructor_type = type(_constructor),extends_type = type(_extends),temp = _extends;
+    var klass = function(_constructor,_extends){
+        var _prop,constructor_type = type(_constructor),extends_type = type(_extends),temp = _extends;
 
-		//Format Arguments
-		_constructor = constructor_type == 'function' ? [_constructor] :
-			constructor_type == 'object' ? (_prop = _constructor) && [_constructor['constructor'] || function(){}] :
-			[function(){}];
-		if(_prop) delete _prop['constructor'];
+        //Format Arguments
+        _constructor = constructor_type == 'function' ? [_constructor] :
+            constructor_type == 'object' ? (_prop = _constructor) && [_constructor['constructor'] || function(){}] :
+            [function(){}];
+        if(_prop) delete _prop['constructor'];
 
-		_extends = extends_type == 'function' ? (_constructor = _constructor.concat(_extends.constructor || _extends)) && _extends.prototype :
-			extends_type == 'object' ? _extends : null;
+        _extends = extends_type == 'function' ? (_constructor = _constructor.concat(_extends.constructor || _extends)) && _extends.prototype :
+            extends_type == 'object' ? _extends : null;
 
-		//Prototype Chain
-		_extends = _extends || {};
-		if(!temp || !temp.extended) _extends.extended = _implement;
+        //Prototype Chain
+        _extends = _extends || {};
+        if(!temp || !temp.extended) _extends.extended = _implement;
 
-		var F = function(){
-			if(_prop) _implement.call(this,_prop);
-		}
-		F.prototype = _extends;
+        var F = function(){
+            if(_prop) _implement.call(this,_prop);
+        }
+        F.prototype = _extends;
 
-		//Constructor
-		var _class = function(){
-			var length = _constructor.length;
-			for(var i = length - 1; i >= 0; i--) _constructor[i].apply(this,arguments);
-		}
-		_class.constructor = _constructor[0];
-		_class.prototype = new F();
+        //Constructor
+        var _class = function(){
+            var length = _constructor.length;
+            for(var i = length - 1; i >= 0; i--) _constructor[i].apply(this,arguments);
+        }
+        _class.constructor = _constructor[0];
+        _class.prototype = new F();
 
-		_class.extended = function(arg){
-			if(!arg.extended) arg.extended = arg.extended || _implement;
-			_constructor = [this.constructor];
-			this.prototype = arg;
+        _class.extended = function(arg){
+            if(!arg.extended) arg.extended = arg.extended || _implement;
+            _constructor = [this.constructor];
+            this.prototype = arg;
 
-			if(type(arg) == 'function'){
-				_constructor = _constructor.concat(arg.constructor || arg);
-				this.prototype = arg.prototype;
-			}
+            if(type(arg) == 'function'){
+                _constructor = _constructor.concat(arg.constructor || arg);
+                this.prototype = arg.prototype;
+            }
 
-			return this;
-		}
-		_class.implements = function(arg){
-			this.prototype = (new F()).extended(arg);
-		}
+            return this;
+        }
+        _class.implements = function(arg){
+            this.prototype = (new F()).extended(arg);
+        }
 
-		return _class;
-	}
+        return _class;
+    }
 
-	var Class = {};
-	Class.create = klass;
-	
+    var Class = {};
+    Class.create = klass;
 
-	return Class;
+
+    return Class;
 })
